f475d333f61bc4e870d4210b8151454e752f0219
26LP99165359 <26LP99165359@G301-CC0196.local>
2026-07-21 12:25:38 +0800

    nemelt

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }],
+  },
 };
 
 export default nextConfig;
